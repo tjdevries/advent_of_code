335553fe551329d8f527502a0db3ee26506718ef
TJ DeVries <devries.timothyj@gmail.com>
2021-12-11 23:44:21 -0500

    day 11: so I can catch up more points against primey

---
+++ 2021/src/day11.rs
@@ -0,0 +1,81 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
+fn step(state: &mut Vec<Vec<u32>>) -> usize {
+    let mut flashed = HashSet::new();
+
+    let rows = state.len();
+    let cols = state[0].len();
+
+    for x in 0..rows {
+        for y in 0..cols {
+            let val = state[x][y];
+
+            if val >= 9 {
+                flashed.insert((x as i32, y as i32));
+            }
+
+            state[x][y] += 1;
+        }
+    }
+
+    for (x, y) in flashed.clone().iter() {
+        flash(state, &mut flashed, *x as i32, *y as i32);
+    }
+
+    for (x, y) in flashed.iter() {
+        state[*x as usize][*y as usize] = 0;
+    }
+
+    flashed.len()
+}
+
+fn flash(state: &mut Vec<Vec<u32>>, flashed: &mut HashSet<(i32, i32)>, x: i32, y: i32) {
+    let rows = state.len() as i32;
+    let cols = state[0].len() as i32;
+
+    for new_x in [x - 1, x, x + 1] {
+        for new_y in [y - 1, y, y + 1] {
+            if new_x == x && new_y == y {
+                continue;
+            }
+
+            if new_x >= rows || new_x < 0 || new_y >= cols || new_y < 0 {
+                continue;
+            }
+
+            if flashed.contains(&(new_x, new_y)) {
+                continue;
+            }
+
+            state[new_x as usize][new_y as usize] += 1;
+            if state[new_x as usize][new_y as usize] > 9 {
+                flashed.insert((new_x, new_y));
+                flash(state, flashed, new_x, new_y);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut state = include_str!("../data/11.input")
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect_vec())
+        .collect_vec();
+
+    println!(
+        "Part 1: {}",
+        (0..100).fold(0, |acc, _| acc + step(&mut state))
+    );
+
+    let mut state = include_str!("../data/11.input")
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect_vec())
+        .collect_vec();
+
+    println!(
+        "Part 2: {}",
+        (0..10000).find(|_| 100 == step(&mut state)).unwrap() + 1
+    );
+}
--- 2021/src/day8.rs
+++ 2021/src/day8.rs
@@ -14,31 +14,6 @@ enum Segments {
     Bottom,
 }
 
-#[derive(Debug, PartialEq, Eq, Hash)]
-enum Numbers {
-    Zero,
-    One,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-}
-
-#[derive(Debug, PartialEq, Eq, Hash)]
-enum Letters {
-    A,
-    B,
-    C,
-    D,
-    E,
-    F,
-    G,
-}
-
 macro_rules! set {
     ($($x:expr),+ $(,)?) => {{
         let mut s = HashSet::new();
@@ -56,85 +31,218 @@ macro_rules! seg_set {
 }
 
 fn main() -> Result<()> {
-    // let puzzle = include_str!("../data/8.example")
-    //     .lines()
-    //     .map(|l| l.split_once(" | ").unwrap().1)
-    //     .flat_map(|output| output.split_whitespace().collect_vec())
-    //     .filter(|display| match display.len() {
-    //         2 | 3 | 4 | 7 => true,
-    //         _ => false,
-    //     })
-    //     .collect_vec()
-    //     .len();
-    //
+    println!(
+        "Part 1: {}",
+        include_str!("../data/8.input")
+            .lines()
+            .map(|l| l.split_once(" | ").unwrap().1)
+            .flat_map(|output| output.split_whitespace().collect_vec())
+            .filter(|display| match display.len() {
+                2 | 3 | 4 | 7 => true,
+                _ => false,
+            })
+            .collect_vec()
+            .len()
+    );
 
-    let one = seg_set!(TopRight, BotRight);
-    let four = seg_set!(TopLeft, Middle, TopRight, BotRight);
-    let seven = seg_set!(Top, TopRight, BotRight);
-    let eight = seg_set!(Top, TopLeft, TopRight, Middle, BotLeft, BotRight, Bottom);
+    let unique_numbers: Vec<HashSet<Segments>> = vec![
+        seg_set!(Top, TopRight, TopLeft, BotLeft, BotRight, Bottom), // 0
+        seg_set!(TopRight, BotRight),                                // 1
+        seg_set!(Top, TopRight, Middle, BotLeft, Bottom),            // 2
+        seg_set!(Top, TopRight, Middle, BotRight, Bottom),           // 3
+        seg_set!(TopRight, Middle, TopLeft, BotRight),               // 4
+        seg_set!(Top, TopLeft, Middle, BotRight, Bottom),            // 5
+        seg_set!(Top, TopLeft, Middle, BotRight, Bottom, BotLeft),   // 6
+        seg_set!(Top, TopRight, BotRight),                           // 7
+        seg_set!(Top, TopLeft, TopRight, Middle, BotLeft, BotRight, Bottom), // 8
+        seg_set!(Top, TopLeft, TopRight, Middle, BotRight, Bottom),  // 9
+    ];
 
-    let all_segments: HashSet<Segments> =
-        seg_set!(Top, TopLeft, TopRight, Middle, BotLeft, BotRight, Bottom);
+    println!(
+        "Part 2: {}",
+        include_str!("../data/8.input")
+            .lines()
+            .fold(0, |acc, line| {
+                // let line = puzzle.next().unwrap();
+                let items = line
+                    .split(" | ")
+                    .map(|p| p.split_whitespace())
+                    .flatten()
+                    .map(|c| c.chars().sorted().collect::<String>())
+                    .collect_vec();
 
-    let mut possibilities = HashMap::new();
-    possibilities.insert('a', all_segments.clone());
-    possibilities.insert('b', all_segments.clone());
-    possibilities.insert('c', all_segments.clone());
-    possibilities.insert('d', all_segments.clone());
-    possibilities.insert('e', all_segments.clone());
-    possibilities.insert('f', all_segments.clone());
-    possibilities.insert('g', all_segments.clone());
+                let all_segments: HashSet<Segments> =
+                    seg_set!(Top, TopLeft, TopRight, Middle, BotLeft, BotRight, Bottom);
 
-    let puzzle = include_str!("../data/8.example")
-        .lines()
-        .next()
-        .unwrap()
-        .split(" | ")
-        .map(|p| p.split_whitespace())
-        .flatten()
-        .collect_vec();
-    println!("{:?}", puzzle);
+                let mut possibilities = HashMap::new();
+                possibilities.insert('a', all_segments.clone());
+                possibilities.insert('b', all_segments.clone());
+                possibilities.insert('c', all_segments.clone());
+                possibilities.insert('d', all_segments.clone());
+                possibilities.insert('e', all_segments.clone());
+                possibilities.insert('f', all_segments.clone());
+                possibilities.insert('g', all_segments.clone());
 
-    for item in puzzle {
-        if item.len() == 2 {
-            println!("{:?}", item);
-            for c in item.chars() {
-                for segment in all_segments.iter() {
-                    if !one.contains(&segment) {
-                        println!(" Removing: {:?}", segment);
-                        possibilities.get_mut(&c).unwrap().remove(&segment);
+                // Find Top.
+                let one = items.iter().find(|item| item.len() == 2).unwrap();
+                let four = items.iter().find(|item| item.len() == 4).unwrap();
+                let seven = items.iter().find(|item| item.len() == 3).unwrap();
+
+                let top = seven.chars().filter(|c| !one.contains(*c)).next().unwrap();
+
+                for possibility in possibilities.values_mut() {
+                    possibility.remove(&Segments::Top);
+                }
+                possibilities.insert(top, seg_set!(Top));
+
+                // Handle One
+                for c in one.chars() {
+                    possibilities.insert(c, seg_set!(TopRight, BotRight));
+                }
+
+                // Handle Three
+                let three = items
+                    .iter()
+                    .find(|item| {
+                        if item.len() != 5 {
+                            return false;
+                        }
+
+                        for c in seven.clone().chars() {
+                            if !item.contains(c) {
+                                return false;
+                            }
+                        }
+
+                        return true;
+                    })
+                    .unwrap();
+
+                for c in 'a'..='g' {
+                    if seven.contains(c) {
+                        continue;
+                    }
+
+                    if !three.contains(c) {
+                        possibilities.get_mut(&c).unwrap().remove(&Segments::Middle);
+                        possibilities.get_mut(&c).unwrap().remove(&Segments::Bottom);
+                        continue;
+                    } else {
+                        if four.contains(c) {
+                            possibilities.insert(c, seg_set!(Middle));
+                        } else {
+                            possibilities.insert(c, seg_set!(Bottom));
+                        }
                     }
                 }
-            }
-        }
-    }
 
-    // let mut careful_examination = HashMap::new();
-    // careful_examination.insert("abcdeg".to_string(), 0);
-    // careful_examination.insert("ab".to_string(), 1);
-    // careful_examination.insert("acdfg".to_string(), 2);
-    // careful_examination.insert("abcdf".to_string(), 3);
-    // careful_examination.insert("abef".to_string(), 4);
-    // careful_examination.insert("bcdef".to_string(), 5);
-    // careful_examination.insert("bcdefg".to_string(), 6);
-    // careful_examination.insert("abd".to_string(), 7);
-    // careful_examination.insert("abcdefg".to_string(), 8);
-    // careful_examination.insert("abcdef".to_string(), 9);
-    //
-    // let puzzle = include_str!("../data/8.example")
-    //     .lines()
-    //     .map(|l| l.split_once(" | ").unwrap().1)
-    //     .map(|output| {
-    //         output
-    //             .split_whitespace()
-    //             .map(|letters| {
-    //                 careful_examination.get(&letters.chars().sorted().collect::<String>())
-    //             })
-    //             .collect_vec()
-    //     })
-    //     .collect_vec();
-    //
-    // println!("{:?}", puzzle);
-    //
+                // Now we know TopLeft
+                for c in 'a'..='g' {
+                    possibilities
+                        .get_mut(&c)
+                        .unwrap()
+                        .remove(&Segments::TopLeft);
+                }
+
+                for c in four.chars() {
+                    if !three.contains(c) {
+                        possibilities.insert(c, seg_set!(TopLeft));
+                    }
+                }
+
+                // BotLeft is remaining only in one
+                let botleft = possibilities
+                    .clone()
+                    .into_iter()
+                    .find(|(_, val)| val.contains(&Segments::BotLeft))
+                    .unwrap();
+
+                possibilities.insert(botleft.0.clone(), seg_set!(BotLeft));
+
+                let nine = items
+                    .iter()
+                    .find(|item| {
+                        if item.len() != 6 {
+                            return false;
+                        }
+
+                        for c in seven.clone().chars() {
+                            if !item.contains(c) {
+                                return false;
+                            }
+                        }
+
+                        for c in four.clone().chars() {
+                            if !item.contains(c) {
+                                return false;
+                            }
+                        }
+
+                        return true;
+                    })
+                    .unwrap();
+
+                let six = items
+                    .iter()
+                    .find(|item| {
+                        if item.len() != 6 {
+                            return false;
+                        }
+
+                        if item == &nine {
+                            return false;
+                        }
+
+                        for c in one.clone().chars() {
+                            if !item.contains(c) {
+                                return true;
+                            }
+                        }
+
+                        return false;
+                    })
+                    .unwrap();
+
+                let topright = one.chars().find(|c| !six.contains(*c)).unwrap();
+
+                for c in 'a'..='g' {
+                    possibilities
+                        .get_mut(&c)
+                        .unwrap()
+                        .remove(&Segments::TopRight);
+                }
+                possibilities.insert(topright, seg_set!(TopRight));
+
+                let mut mapping = HashMap::new();
+                for (c, p) in possibilities.iter() {
+                    mapping.insert(c, p.iter().next().unwrap());
+                }
+
+                let outputs = line
+                    .split_once(" | ")
+                    .unwrap()
+                    .1
+                    .split_whitespace()
+                    .map(|item| {
+                        item.chars().fold(HashSet::new(), |mut acc, c| {
+                            acc.insert(*mapping.get(&c).unwrap().clone());
+                            acc
+                        })
+                    })
+                    .map(|mapped| {
+                        for (idx, unique) in unique_numbers.iter().enumerate() {
+                            if *unique == mapped {
+                                return idx;
+                            }
+                        }
+
+                        panic!("we done messed up, but rust is safe");
+                    })
+                    .fold(0, |acc, val| acc * 10 + val);
+
+                acc + outputs
+            })
+    );
+
     Ok(())
 }
